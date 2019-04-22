c7d892c0d712f1eee4e5a3ee5f0f2dd737d87675
Alimur4ikS9 <alinur.sabit.9@gmail.com>
2019-04-22 12:00:52 +0600

    start django

---
+++ week 12/todo-back/env/Scripts/activate.bat
@@ -0,0 +1,30 @@
+@echo off
+set "VIRTUAL_ENV=D:\01_ALI~1\KBTU_3.2\WEBDEV~1\WEE1A3~1\WEBTEC~1\WEEK12~1\TODO-B~1\env"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+    set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+)
+set "PROMPT=(env) %PROMPT%"
+
+REM Don't use () to avoid problems with them in %PATH%
+if defined _OLD_VIRTUAL_PYTHONHOME goto ENDIFVHOME
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+:ENDIFVHOME
+
+set PYTHONHOME=
+
+REM if defined _OLD_VIRTUAL_PATH (
+if not defined _OLD_VIRTUAL_PATH goto ENDIFVPATH1
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+:ENDIFVPATH1
+REM ) else (
+if defined _OLD_VIRTUAL_PATH goto ENDIFVPATH2
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+:ENDIFVPATH2
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
---
+++ week 12/todo-back/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\01_ali~1\kbtu_3.2\webdev~1\wee1a3~1\webtec~1\week12~1\todo-b~1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
